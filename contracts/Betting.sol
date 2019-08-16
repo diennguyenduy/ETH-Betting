1bfff422942871d79460f21047c3efcb52f17bfa
dien <diennguyenduy1311999@gmail.com>
2019-08-16 10:47:20 +0700

    update contract

--- contracts/Betting.sol
+++ contracts/Betting.sol
@@ -1,7 +1,12 @@
-pragma solidity ^0.4.23;
+pragma solidity 0.5.11;
 
 contract Betting {
-    address public owner;
+
+    /*
+    * Storage
+    */
+
+    address payable public owner;
     uint public lastWinnerNumber;
     uint public betValue = 1 ether;
     uint public totalBet;
@@ -9,23 +14,30 @@ contract Betting {
     uint public totalSlots = 4;
     address[] public players;
 
-    mapping(uint => address[]) public numberToPlayers;
+    mapping(uint => address payable[]) public numberToPlayers;
     mapping(address => uint) public playerToNumber;
 
-    constructor () public {
-        owner = msg.sender;
-    }
+    /*
+    * Modifiers
+    */
 
     modifier validBet(uint betNumber) {
-        require(playerToNumber[msg.sender] == 0);
-        require(msg.value >= betValue);
-        require(numberOfBets < 10);
-        require(betNumber >= 1 && betNumber <= 10);
+        require(playerToNumber[msg.sender] == 0, "Only can bet once");
+        require(msg.value >= betValue, "Not enough balance!");
+        require(numberOfBets < 10, "Only 9 players");
+        require(betNumber >= 1 && betNumber <= 10, "Only in range 1 and 10");
         _;
     }
 
-    function bet(uint betNumber) public payable validBet(betNumber) {
-        require(msg.sender.balance >= msg.value);
+    constructor(address payable _owner) public {
+        owner = _owner;
+    }
+
+    function bet(uint betNumber)
+        public
+        payable
+        validBet(betNumber) {
+        require(msg.sender.balance >= msg.value, "Not enough balance to excute transaction!");
         if (msg.value > betValue) {
             msg.sender.transfer(msg.value - betValue);
         }
@@ -41,19 +53,22 @@ contract Betting {
 
     function distributePrizes() internal {
         uint winnerNumber = generateRandomNumber();
-        address[] memory winners = numberToPlayers[winnerNumber];
+        address payable[] memory winners = numberToPlayers[winnerNumber];
         if (winners.length > 0) {
-            uint winnerEtherAmount = totalBet / winners.length;
+            uint winnerEtherAmount = ((totalBet/100) * 80) / winners.length;
+            owner.transfer((totalBet/100)*20);
             for (uint i = 0; i < numberToPlayers[winnerNumber].length; i++) {
                 numberToPlayers[winnerNumber][i].transfer(winnerEtherAmount);
             }
+        } else {
+            owner.transfer(totalBet);
         }
         lastWinnerNumber = winnerNumber;
         reset();
     }
 
     function generateRandomNumber() internal view returns (uint) {
-        return (block.number % 10 + 1);
+        return ((block.number + block.timestamp + block.difficulty) % 10 + 1);
     }
 
     function reset() internal {
@@ -71,7 +86,7 @@ contract Betting {
     }
 
     function kill() public {
-        require(msg.sender == owner);
+        require(msg.sender == owner, "Only owner call call this!");
         selfdestruct(owner);
     }
 }
